5ecc67925837ace2baf056734fde1f42dd54c01e
Jakob-Peter <jakob.peter@syntax-institut.de>
2025-01-21 13:59:05 +0100

    Initial Commit

---
+++ CharacterApp/CharacterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterAppApp.swift
+//  CharacterApp
+//
+//  Created by Jakob Peter on 21.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CharacterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
